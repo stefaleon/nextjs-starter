69e9d6f264b818859f80d6e51a324d77946bebc0
unknown <stefaleon@gmail.com>
2021-05-20 05:12:15 +0300

    metadata

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 https://nextjs.org/learn/basics/assets-metadata-css/assets
 
 http://localhost:3000/profile
+
+## metadata
+
+https://nextjs.org/learn/basics/assets-metadata-css/metadata
+
+http://localhost:3000/profile
--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Image from 'next/image';
 
 export default function profile() {
   return (
     <div>
+      <Head>
+        <title>Profile</title>
+      </Head>
       <Image
         src="/images/batmanikos.png" // Route of the image file
         height={144} // Desired size with correct aspect ratio
